067fd61f5a098f8dee5ff56172afa5dd0490daa0
BigDog4js <bigdog4js@yahoo.com>
2017-06-09 16:01:56 -0500

    Second commit, fixed infinite rotater which stoped because jquery loaded before angular

--- app.js
+++ app.js
@@ -8,7 +8,40 @@ angular.module("app",["ui.router"]).config(function($stateProvider, $urlRouterPr
     })
     .state("home", {
         url: '/',
-        templateUrl: "./templates/home.html"
+        templateUrl: "./templates/home.html",
+        controller: function () {
+            var initialFadeIn = 1000;
+			
+			//interval between items (in milliseconds)
+			var itemInterval = 3000;
+			
+			//cross-fade time (in milliseconds)
+			var fadeTime = 2000;
+			
+			//count number of items
+			var numberOfItems = $('.rotating-item').length;
+
+			//set current item
+			var currentItem = 0;
+
+			//show first item
+			$('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+
+			//loop through the items		
+			var infiniteLoop = setInterval(function(){
+				$('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+
+				if(currentItem == numberOfItems -1){
+					currentItem = 0;
+				}else{
+					currentItem++;
+				}
+				$('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+
+			}, itemInterval);	
+			
+
+        }
     })
 
 
